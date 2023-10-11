c4531636bc1491c7ca541796c4b4378f45ba2739
Sean Miller <sean@seanmiller.ca>
2023-10-11 13:36:09 -0400

    Add @REQUIREMENT in related repo for validating labels match

--- spec/relies_on_related_repo_spec.rb
+++ spec/relies_on_related_repo_spec.rb
@@ -1,6 +1,12 @@
-RSpec.describe "relies on related repo" do
-  describe "my_hounds from relies_on_example" do
+RSpec.describe "'@RELIES_ON: ' and '@REQUIREMENT: ' in related repo" do
+  describe "@RELIES_ON: <repo:relies_on_example_two> (for validating related repo links show up in 'Other specs rely on this requirement')" do
     # @RELIES_ON: <repo:relies_on_example_two>:my_hounds includes :breeding_stock
+    it "does something" do
+      expect(true).to be true
+    end
+  end
+  describe "@REQUIREMENT (for validating labels match requirements in related repos)" do
+    # @REQUIREMENT: A requirement in a related repo
     it "does something" do
       expect(true).to be true
     end
